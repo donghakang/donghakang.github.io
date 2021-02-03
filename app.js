c5db75af61a0fe0196895d22a032fdc6dd9f5d94
Dongha Kang <dkang0602@gmail.com>
2021-02-03 22:15:40 +0900

    three js module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://donghakang.github.io/node_modules/three/build/three.module.js";
+import * as THREE from "./node_modules/three/build/three.module.js";
 
 function main() {
     // scene
