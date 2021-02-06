187ddaf6ed546e2ecc53dd986000d46ed0e6ff59
Dongha Kang <dkang0602@gmail.com>
2021-02-06 09:48:50 +0900

    separate components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import Footer from "./components/footer";
+
 import * as THREE from "three";
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useMemo, useState } from "react";
 import "./App.scss";
 
 import { Canvas, useFrame } from "react-three-fiber";
@@ -7,8 +9,10 @@ import {
   softShadows,
   OrbitControls,
   MeshWobbleMaterial,
-  Html
+  useGLTF,
+  Html,
 } from "@react-three/drei";
+import { Material } from "three";
 
 softShadows();
 
@@ -16,12 +20,12 @@ const Light = () => {
   return (
     <group>
       <ambientLight intensity={0.5} />
-      <pointLight position={[-10, 0, -20]} intensity={0.5} />
-      <pointLight position={[0, -10, 0]} intensity={1.5} />
+      <pointLight position={[-5, 2, -10]} intensity={0.5} />
+      <pointLight position={[-10, 3, -5]} intensity={1.5} />
       <directionalLight
         castShadow
         position={[0, 10, 0]}
-        intensity={1.0}
+        intensity={2.0}
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
         shadow-camera-far={50}
@@ -35,51 +39,42 @@ const Light = () => {
   );
 };
 
-const SpinningMesh = ({ position, size, color, speed }) => {
+const Ground = () => {
+  return (
+    <group>
+      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+        <planeBufferGeometry attach="geometry" args={[100, 100]} />
+        <shadowMaterial attach="material" opacity={0.4} />
+      </mesh>
+    </group>
+  );
+};
+
+const Model = () => {
+  const { nodes, scene } = useGLTF("/scene.gltf");
+  console.log(nodes, scene);
   const mesh = useRef(null);
+  const gltf = useGLTF("/scene.gltf");
 
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+  gltf.scene.children.forEach((mesh, i) => {
+    mesh.castShadow = true;
+  });
+  gltf.castShadow = true;
+  gltf.scene.castShadow = true;
 
-  const vec = new THREE.Vector3(2, 2, 2);
-  const vec_origin = new THREE.Vector3(1, 1, 1);
   useFrame(() => {
-    if (hovered) {
-      mesh.current.scale.lerp(vec, 0.1);
-    } else {
-      mesh.current.scale.lerp(vec_origin, 0.1);
-    }
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    mesh.current.rotation.y += 0.005;
   });
 
   return (
-    <mesh
-      castShadow
-      position={position}
-      ref={mesh}
-      scale={[1,1,1]}
-      onClick={(event) =>
-        (window.location.href = "https://www.github.com/donghakang")
-      }
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <boxBufferGeometry attach="geometry" args={size} />
-      {/* <meshStandardMaterial attach="material" color={color} /> */}
-      <MeshWobbleMaterial
-        attach="material"
-        color={hovered ? { color } : "green"}
-        speed={speed}
-        factor={0.6}
-      />
-      <Html scaleFactor={10}>
-        <div class="content">
-          Suspense <br />
-          Dongha Kang
-        </div>
-      </Html>
+    <mesh castShadow ref={mesh} position={[0, 0, 0]}>
+      <primitive object={gltf.scene} dispose={null} />
+
     </mesh>
   );
+
+
+  
 };
 
 function App() {
@@ -88,29 +83,16 @@ function App() {
       <Canvas
         shadowMap
         colorManagement
-        camera={{ position: [-5, 2, 10], fov: 60 }}
+        camera={{ position: [-1, 1, 1], fov: 70 }}
       >
         <Light />
-        <group>
-          <mesh
-            receiveShadow
-            rotation={[-Math.PI / 2, 0, 0]}
-            position={[0, -3, 0]}
-          >
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <shadowMaterial attach="material" opacity={0.4} />
-          </mesh>
-        </group>
-        <SpinningMesh
-          position={[0, 1, 0]}
-          size={[3, 1, 2]}
-          color="green"
-          speed={4}
-        />
-        <SpinningMesh position={[-2, 1, -5]} color="pink" speed={1} />
-        <SpinningMesh position={[5, 1, -2]} color="pink" speed={1} />
+        <Ground />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
         <OrbitControls />
       </Canvas>
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.github.com/donghakang">github</a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/dkang0602">facebook</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/donghakang">instagram</a>
+          </li>
+          <li>
+            <a href="dkang0602@gmail.com">mail</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
