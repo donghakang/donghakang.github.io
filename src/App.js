6ae2e6f135e4cc8e7c99a34f12487a7f0084f28b
Dongha Kang <dkang0602@gmail.com>
2021-02-19 22:42:06 +0900

    on pointer down HTML pop up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Footer from "./components/footer";
 
-import * as THREE from "three";
-import React, { Suspense, useRef, useEffect } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import "./App.scss";
 
 import { Canvas, useFrame, useThree } from "react-three-fiber";
+import { useSpring, animated } from "react-spring";
 import {
   OrbitControls,
   useGLTF,
   softShadows,
-  MeshWobbleMaterial
+  Html
 } from "@react-three/drei";
-import { MeshPhongMaterial } from "three";
+
 
 
 softShadows();
@@ -64,29 +64,42 @@ function Mac(props) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/mac.gltf')
 
+  const [active, setActive] = useState(false)
+  const animate = useSpring({ opacity: active ? 1 : 0 })
+
   useFrame(() => {
     group.current.rotation.x += 0.02
     group.current.rotation.y += 0.01
   })
 
   return (
-    <group position={[20, 0, 10]}>
-      <group ref={group} {...props} dispose={null}>
-        <group position={[-4.7, 0, 0]} scale={[.01, .01, .01]}>
-          <mesh
-            castShadow
-            material={
-              materials['Mac.Tri.Baked']}
-            geometry={nodes.Mac_Tri_Baked_MacTriBaked_0.geometry}
-          >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2} />
-          </mesh>
-        </group>
-      </group >
+    <group
+      onPointerDown={(e) => setActive(!active)}
+    >
+      <group position={[20, 0, 10]} >
+        <group ref={group} {...props} dispose={null}>
+          <group position={[-4.7, 0, 0]} scale={[.01, .01, .01]}>
+            <mesh
+              castShadow
+              material={
+                materials['Mac.Tri.Baked']}
+              geometry={nodes.Mac_Tri_Baked_MacTriBaked_0.geometry}
+            >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2} />
+              <Html scaleFactor={80}>
+                <animated.div style={animate} className="content">
+                  PROJECT
+                </animated.div>
+              </Html>
+            </mesh>
+          </group>
+        </group >
+      </group>
+
     </group>
   )
 }
@@ -95,21 +108,26 @@ function Statue(props) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/statue.gltf')
 
+  const [active, setActive] = useState(false)
+  const animate = useSpring({ opacity: active ? 1 : 0 })
+
   useFrame(() => {
     group.current.rotation.x += 0.02
   })
 
+  // TODO: clickable change
   return (
     <group position={[0, 0, -15]}>
-      <group  {...props} dispose={null}>
+      <group ref={group}  {...props} dispose={null}>
         <group position={[4.6, 6.8, -8.4]} rotation={[Math.PI / 2, -Math.PI / 2, Math.PI / 8]}>
-        <group ref={group}>
+          <group
+            onPointerDown={(e) => setActive(!active)} >
 
-        <mesh scale={[10,-12,12]}>
-          <boxBufferGeometry position={[-1.2, 0, 0]} />
-          <meshStandardMaterial color="green" opacity={0.2} />
-        </mesh>
-        </group>
+            <mesh position={[10, 6.5, 5]} rotation={[0, 0, -Math.PI / 8]} scale={[20, 10, 10]}>
+              <boxBufferGeometry position={[0, 0, 0]} />
+              <meshStandardMaterial transparent={true} opacity={0.0} />
+            </mesh>
+          </group>
           <mesh castShadow material={materials.material_0} geometry={nodes.mesh_0.geometry}>
             <meshStandardMaterial
               attach="material"
@@ -200,144 +218,170 @@ function Statue(props) {
           </mesh>
         </group>
       </group>
+      <Html scaleFactor={80}>
+        <animated.div style={animate} className="content">
+          ABOUT ME
+                </animated.div>
+      </Html>
     </group>
   )
 }
 
+
+
+
 function Phone(props) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/phone.gltf')
+
+  const [active, setActive] = useState(false)
+  const animate = useSpring({ opacity: active ? 1 : 0 })
   useFrame(() => {
-    group.current.rotation.y += 0.1
+    // group.current.rotation.y += 0.1
   })
 
   return (
+    
     <group position={[-20, 0, 10]}>
       <group ref={group} {...props} dispose={null}>
-        <group scale={[.7, .7, .7]} position={[0, 0, 2.5]} rotation={[Math.PI / 2, 0, 0]}>
-          <mesh castShadow material={materials.material_0} geometry={nodes.mesh_0.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials.Phone_body} geometry={nodes.mesh_1.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials.Ariel} geometry={nodes.mesh_2.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.002']} geometry={nodes.mesh_3.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials.phone_Face} geometry={nodes.mesh_4.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.003']} geometry={nodes.mesh_5.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials.Screen} geometry={nodes.mesh_6.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.004']} geometry={nodes.mesh_7.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.005']} geometry={nodes.mesh_8.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.010']} geometry={nodes.mesh_9.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.006']} geometry={nodes.mesh_10.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.007']} geometry={nodes.mesh_11.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.008']} geometry={nodes.mesh_12.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials['Material.009']} geometry={nodes.mesh_13.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
-          <mesh castShadow material={materials.SVGMat} geometry={nodes.mesh_14.geometry} >
-            <meshStandardMaterial
-              attach="material"
-              color="white"
-              roughness={0.3}
-              metalness={0.2}
-            />
-          </mesh>
+          <group scale={[.7, .7, .7]} position={[0, 0, 2.5]} rotation={[Math.PI / 2, 0, 0]}>
+          <group
+            onPointerDown={(e) => setActive(!active)} >
+            <mesh position={[0, -5, 0]} rotation={[0, 0, 0]} scale={[10,10,30]}>
+              <boxBufferGeometry position={[0, 0, 0]} />
+              <meshStandardMaterial transparent={true} opacity={0.0} />
+            </mesh>
+          </group>
+            <mesh castShadow material={materials.material_0} geometry={nodes.mesh_0.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials.Phone_body} geometry={nodes.mesh_1.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials.Ariel} geometry={nodes.mesh_2.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.002']} geometry={nodes.mesh_3.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials.phone_Face} geometry={nodes.mesh_4.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.003']} geometry={nodes.mesh_5.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials.Screen} geometry={nodes.mesh_6.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.004']} geometry={nodes.mesh_7.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.005']} geometry={nodes.mesh_8.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.010']} geometry={nodes.mesh_9.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.006']} geometry={nodes.mesh_10.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.007']} geometry={nodes.mesh_11.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.008']} geometry={nodes.mesh_12.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials['Material.009']} geometry={nodes.mesh_13.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+            <mesh castShadow material={materials.SVGMat} geometry={nodes.mesh_14.geometry} >
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                roughness={0.3}
+                metalness={0.2}
+              />
+            </mesh>
+          </group>
+          
         </group>
+        <Html scaleFactor={80}>
+                <animated.div style={animate} className="content">
+                  CONTACT
+                </animated.div>
+              </Html>
       </group>
-    </group>
+
   )
 }
 
@@ -352,11 +396,9 @@ function Objects(props) {
 
   return (
     <group ref={obj}>
-      <Mac
-        onPointerDown={(e) => console.log("Mac Clicked")}
-      />
-      <Statue onPointerDown={(e) => console.log("Statue clicked")} />
-      < Phone onPointerDown={(e) => console.log("Phone clicked")} />
+      <Mac />
+      <Statue />
+      < Phone />
     </group>
   )
 }
