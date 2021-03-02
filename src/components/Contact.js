67a7c341d174d372e9ee3598e57b1c91f98d4a50
Dongha Kang <dkang0602@gmail.com>
2021-03-02 22:44:20 +0900

    add a line (practice css)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,42 @@
-import Nav from './Nav'
+import Nav from "./Nav";
 
-import { FaGithub, FaFacebook, FaInstagram } from 'react-icons/fa';
-import { FiMail } from 'react-icons/fi';
+import { FaGithub, FaFacebook, FaInstagram } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
 
 import "../App.scss";
 
 export default function Contact() {
-    const m_size = 40;
-    const m_style = { color: '#dddddd' };
-    return (
-        
-        <div>
-            <Nav />
-            <div className={'contact'}> 
-                <ul>
-                    <li>
-                        <a href="https://www.github.com/donghakang"><FaGithub size={m_size} style={m_style}/></a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/donghakang"><FaFacebook size={m_size} style={m_style}/></a>
-                    </li>
-                    <li>
-                        <a href="https://www.facebook.com/dkang0602"><FaInstagram size={m_size} style={m_style}/></a>
-                    </li>
-                    <li>
-                        <a href="mailto:dkang0602@gmail.com"><FiMail size={m_size} style={m_style}/></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+  const m_size = 60;
+  const m_style = { color: "#dddddd" };
+  return (
+    <>
+      <Nav />
+      <div className={"contact"}>
+        <a href="https://www.github.com/donghakang">
+          <div>
+            <FaGithub size={m_size} style={m_style} />
+          </div>
+          <span>Github</span>
+        </a>
+        <a href="https://www.instagram.com/donghakang">
+          <div>
+            <FaInstagram size={m_size} style={m_style} />
+          </div>
+          <span>Instagram</span>
+        </a>
+        <a href="https://www.facebook.com/dkang0602">
+          <div>
+            <FaFacebook size={m_size} style={m_style} />
+          </div>
+          <span>facebook</span>
+        </a>
+        <a href="mailto:dkang0602@gmail.com">
+          <div>
+            <FiMail size={m_size} style={m_style} />
+          </div>
+          <span>mail</span>
+        </a>
+      </div>
+    </>
+  );
 }
