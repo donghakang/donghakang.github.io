3c2c5f6e060497a176cd1ddf19f4a3dfca25ea2e
Dongha Kang <dkang0602@gmail.com>
2021-08-02 01:21:58 +0900

    🚀 Timeline added
    - Study CSS commands (::before ::after)

--- src/components/about/Work.js
+++ src/components/about/Work.js
@@ -8,30 +8,19 @@ function Work() {
         <ul>
           <li>
             <div className="timeline-content">
-              <h1>Heading 1</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aspernatur esse blanditiis facilis soluta, doloremque molestias
-                ea veniam qui tempora nihil officia, voluptas quaerat ullam
-                aliquam vitae recusandae culpa, dicta ab!
-              </p>
+              <h4>Cuyahoga Falls Christian Academy</h4>
+              <p className="timeline-paragraph">On my way to the U.S.</p>
             </div>
           </li>
           <li>
             <div className="timeline-content">
               <h1>Heading 2</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aspernatur esse blanditiis facilis soluta, doloremque molestias
-                ea veniam qui tempora nihil officia, voluptas quaerat ullam
-                aliquam vitae recusandae culpa, dicta ab!
-              </p>
             </div>
           </li>
           <li>
             <div className="timeline-content">
               <h1>Heading 3</h1>
-              <p>
+              <p className="timeline-paragraph">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Aspernatur esse blanditiis facilis soluta, doloremque molestias
                 ea veniam qui tempora nihil officia, voluptas quaerat ullam
@@ -42,7 +31,7 @@ function Work() {
           <li>
             <div className="timeline-content">
               <h1>Heading 4</h1>
-              <p>
+              <p className="timeline-paragraph">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Aspernatur esse blanditiis facilis soluta, doloremque molestias
                 ea veniam qui tempora nihil officia, voluptas quaerat ullam
