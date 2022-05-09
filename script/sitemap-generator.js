a2e726b850c81aedb7b4933949baebba0d70d914
Dongha Kang <dkang0602@gmail.com>
2022-05-09 11:35:12 +0900

    :bug: fix: sitemap fix

--- script/sitemap-generator.js
+++ script/sitemap-generator.js
@@ -1,30 +1,29 @@
-const fs = require("fs");
-const globby = require("globby");
-require("dotenv").config({ path: ".env" });
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const fs = require("fs")
+const globby = require("globby")
+require("dotenv").config({ path: ".env" })
+
+const WEBSITE_URL = "https://donghakang.github.io"
 
 function addPage(page) {
   const path = page
     .replace("pages", "")
     .replace(".tsx", "")
-    .replace("/index", "");
+    .replace("/index", "")
 
-  return `  <url>
-    <loc>${`${process.env.WEBSITE_URL}${path}`}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
-  </url>`;
+  return `<url>
+  <loc>${`${WEBSITE_URL}${path}`}</loc>
+  <lastmod>${new Date().toISOString()}</lastmod>
+</url>`
 }
 
 function addBlogPage(page) {
-  const path = page.replace(".mdx", "");
+  const path = page.replace(".mdx", "")
 
-  return `  <url>
-    <loc>${`${process.env.WEBSITE_URL}/${path}`}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>1.0</priority>
-  </url>`;
+  return `<url>
+  <loc>${`${WEBSITE_URL}/${path}`}</loc>
+  <lastmod>${new Date().toISOString()}</lastmod>
+</url>`
 }
 
 async function generateSitemap() {
@@ -36,18 +35,21 @@ async function generateSitemap() {
     "!pages/404.tsx",
     "!pages/blog/[slug].tsx",
     "!pages/project/[project_slug].tsx",
-  ]);
+  ])
 
-  const blog = await globby(["blog/*.mdx", "project/*.mdx"]);
+  const blog = await globby(["blog/*.mdx", "project/*.mdx"])
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${pages.map(addPage).join("\n")}`;
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+>\n${pages.map(addPage).join("\n")}`
 
-  const blogSitemap = `${blog.map(addBlogPage).join("\n")}</urlset>`;
+  const blogSitemap = `\n${blog.map(addBlogPage).join("\n")}\n</urlset>`
 
-  console.log("build sitemap..");
-  fs.writeFileSync("public/sitemap.xml", sitemap + blogSitemap);
+  console.log("build sitemap..")
+  fs.writeFileSync("public/sitemap.xml", sitemap + blogSitemap)
 }
 
-generateSitemap();
+generateSitemap()
