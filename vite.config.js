d287d9f9a66d960f6fdab65ccf8d35eadc8349c3
donghakang <dkang0602@gmail.com>
2023-08-26 14:48:37 +0900

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(() => {
   return {
     // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
+    base: '/',
     define: {
       'process.env': {},
     },
