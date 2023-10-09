88206daeb36aff7b29cb2972a201bd840cf68223
donghakang <dkang0602@gmail.com>
2023-10-09 21:48:23 +0900

    feat: ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,40 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { Inter, Cousine } from 'next/font/google'
 import cx from 'classnames'
+import type { AppProps } from 'next/app'
+import { Cousine, Inter } from 'next/font/google'
+import Script from 'next/script'
 
 export const inter = Inter({ subsets: ['latin'] })
 export const cousine = Cousine({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={cx(inter.className)}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </main>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <main className={cx(inter.className)}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </>
   )
 }
