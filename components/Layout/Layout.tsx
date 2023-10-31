0adb582c8329d396471bd66a5f61faf6a0eb0056
donghakang <dkang0602@gmail.com>
2023-10-31 20:10:38 +0900

    fix: layout and new playlist

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import styles from './layout.module.scss'
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      {children}
+      <div className={styles.BodyContainer}>{children}</div>
     </>
   )
 }
